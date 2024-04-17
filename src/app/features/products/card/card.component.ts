d87f3be8a8800ee43d755acdf312becef3b6c0d0
Minuke <nakamadeath@gmail.com>
2024-04-17 18:43:44 +0200

    navigation between products and details

--- src/app/features/products/card/card.component.ts
+++ src/app/features/products/card/card.component.ts
@@ -1,11 +1,12 @@
 import { SlicePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '@shared/models/product.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [SlicePipe],
+  imports: [SlicePipe, RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
